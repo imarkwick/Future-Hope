3b802e00bd73722cc6ab90c3f5e6a392843b11bf
imarkwick <izzymarkwick@gmail.com>
2015-04-20 17:10:42 +0100

    image folder

--- app/server.rb
+++ app/server.rb
@@ -7,13 +7,16 @@ require_relative 'data_mapper_setup'
 set :public_dir, Proc.new { File.join(root, "..", "public") }
 
 get '/' do 
+
 	erb :index
 end
 
 get '/volunteer' do
+
 	erb :volunteer
 end
 
 get '/display' do
+
 	erb :display
 end
