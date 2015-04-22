0e31f3ca7a7687bc4a1709f500ea8dc832b03c96
clare trembath <claretrembath@ymail.com>
2015-04-22 12:19:06 +0100

    failing needs radio buttons

--- spec/adding_item_spec.rb
+++ spec/adding_item_spec.rb
@@ -12,10 +12,20 @@ feature 'Volunteer adds guest names to an item' do
 		expect(item.total).to eq('5')
 	end	
 
-	def add_item(title, total)
+	scenario 'with some guest names' do
+		visit '/volunteer'
+		add_item('Sponsor A Child', '5');
+		item = Item.first
+		expect(item.names).to include('Peter')
+		expect(item.names).to include('Izzy')
+	end
+
+	def add_item(title, total, names = [])
 		within('#new-item') do
 			select 'Sponsor A Child', :from => "title"
 			fill_in 'total',with: total
+			choose('Peter')
+			choose('Izzy')
 			click_button 'Submit'
 		end
 	end	
