de7c05f0605354b94c1f21770a37eacdda442e7f
imarkwick <izzymarkwick@gmail.com>
2015-04-28 16:25:11 +0100

    `playing with sockets

--- app/server.rb
+++ app/server.rb
@@ -13,6 +13,27 @@ set :session_secret, 'super secret'
 set :partial_template_engine, :erb
 set :public_dir, Proc.new { File.join(root, "..", "public") }
 
+# def websocket_connection
+# 	Faye::WebSocket.websocket?(request.env)
+# 	ws = Faye::WebSocket.new(request.env)
+
+# 	ws.on(:open) do |event|
+# 		puts 'On Open'
+# 	end
+
+# 	ws.on(:message) do |msg|
+# 		puts "!!!!!!!!!!!!"
+# 		ws.send(msg.data)
+# 		puts msg.data
+# 	end
+
+# 	ws.on(:close) do |event|
+# 		puts 'On Close'
+# 	end
+
+# 	ws.rack_response
+# end
+
 get '/' do 
 	erb :index
 end
@@ -41,6 +62,7 @@ get '/volunteer' do
 			@all_table_names = table_guestlist(@table)
 		end
 	end
+
 	if Faye::WebSocket.websocket?(request.env)
 		ws = Faye::WebSocket.new(request.env)
 
@@ -49,7 +71,8 @@ get '/volunteer' do
 		end
 
 		ws.on(:message) do |msg|
-			ws.send(msg.data.reverse)
+			puts "!!!!!!!!!!!!"
+			ws.send("****#{msg.data}")
 			puts msg.data
 		end
 
@@ -61,7 +84,6 @@ get '/volunteer' do
 	else
 		erb :volunteer
 	end
-	# erb :volunteer
 end
 
 post '/volunteer' do
@@ -70,14 +92,38 @@ post '/volunteer' do
 end
 
 get '/display' do
+
+
 	@items = Item.all
 	items_and_totals = images_per_item(@items)
 	total_items = total_images(items_and_totals)
 	list = total_items.join('')
 	@array = list.split(',')
 	@array.delete(@array.last)
-	@array.each { |image| image[0] = '' if image[0] == ' ' }
-	erb :display
+	@array.each { |image| image[0] = '' if image[0] == ' ' }	
+
+	if Faye::WebSocket.websocket?(request.env)
+		ws = Faye::WebSocket.new(request.env)
+
+		ws.on(:open) do |event|
+			puts 'On Open ******'
+		end
+
+		ws.on(:message) do |msg|
+			puts "**********"
+			ws.send(msg.data)
+			puts msg.data
+		end
+
+		ws.on(:close) do |event|
+			puts 'On Close'
+		end
+
+		ws.rack_response
+	else
+		erb :display
+	end
+	# erb :display
 end
 
 post '/display' do
