80e1867fe967cf22766bddf6e63252a06345ec8f
imarkwick <izzymarkwick@gmail.com>
2015-04-28 12:51:24 +0100

    config

--- app/server.rb
+++ app/server.rb
@@ -51,13 +51,12 @@ end
 
 get '/display' do
 	@items = Item.all
-	# items_and_totals = images_per_item(@items)
-	# total_items = total_images(items_and_totals)
-	# list = total_items.join('')
-	# @array = list.split(',')
-	# @array.delete(@array.last)
-	# @array.each { |image| image[0] = '' if image[0] == ' ' }
-
+	items_and_totals = images_per_item(@items)
+	total_items = total_images(items_and_totals)
+	list = total_items.join('')
+	@array = list.split(',')
+	@array.delete(@array.last)
+	@array.each { |image| image[0] = '' if image[0] == ' ' }
 	if Faye::WebSocket.websocket?(request.env)
 		ws = Faye::WebSocket.new(request.env)
 
@@ -77,6 +76,7 @@ get '/display' do
 	else
 		erb :display
 	end
+	# erb :display
 end
 
 post '/display' do
